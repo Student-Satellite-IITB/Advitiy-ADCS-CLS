fcf43b088962da41597478a9bcd6fa43787e03a2
RIYA <rsriya383@gmail.com>
2019-03-12 18:09:34 +0530

    Adding the QAed files
    after implementation of QA suggestions

--- solver.py
+++ solver.py
@@ -1,35 +1,34 @@
 import numpy as np
-import frames
-from constants_1U import G, M_EARTH, v_w_IO_o
 
-def rk4Quaternion(sat,f,h): #This is Runge Kutta-4 solver for ordinary differential equation.
+def updateStateTimeRK4(sat,f,h): #This is Runge Kutta-4 solver for ordinary differential equation.
 	'''
-		Input is satellite object, f (derivative of state vector (quaternion and angular velocity)) and integration step size
-		It returns value of state at next time (after a time step of h) (x(t+h)) using f and value of state at current time (x(t))
-	
-	print(sat.getQ_BI())
-	print(sat.getW_BI_b())
-	print(sat.getControl_b())
-	print(sat.getDisturbance_b())
-	print(sat.getVel())
-	print(sat.getPos())
+		Input is satellite object, f: The function returning the derivative of the state given satellite object as its input; and integration step size
+		It sets the value of state at next time (after a time step of h) (x(t+h)) using f and value of state at current time (x(t))
+		and also updates the value of time.
 	'''
 	v_state_error_0 = sat.getState()	#state at t = t0	
 	t = sat.getTime() 
+
+	#checking whether the f is a proper function or not
+	if (np.shape(f(sat)) != (7,)):
+		return
 	#rk-4 routine (updating satellite class state with obtained state at every step of rk4 routine)
 	#first step of rk4 routine
 	k1 = h*f(sat)
 	#second step of rk4 routine
 	v_state_error_1 = v_state_error_0+0.5*k1
+	sat.setTime(t+0.5*h)
 	sat.setState(v_state_error_1)
 
 	k2 = h*f(sat)
 	#third step of rk4 routine	
 	v_state_error_2 = v_state_error_0+0.5*k2
+	sat.setTime(t+0.5*h)
 	sat.setState(v_state_error_2)
 
 	k3 = h*f(sat)
 	v_state_error_3 = v_state_error_0+k3
+	sat.setTime(t+h)
 	sat.setState(v_state_error_3)
 
 	#forth step of rk4 routine
--- test_solver.py
+++ test_solver.py
@@ -1,14 +1,16 @@
-from solver import rk4Quaternion
+from solver import updateStateTimeRK4
 import numpy as np
 import frames
 import unittest
 import satellite
 from dynamics import x_dot_BO
 from constants_1U import G, M_EARTH, v_w_IO_o
+from ddt import data, ddt
 
 def f_constant(sat):
-	return 1.0
-
+	cons = np.array((1.,1.,1.,1.,1.,1.,1.))
+	return cons
+@ddt
 class TestSolver(unittest.TestCase):
 	
 	def test_constant(self):
@@ -19,13 +21,14 @@ class TestSolver(unittest.TestCase):
 		mySat.setVel(np.array([5.0,5.0,0.0]))
 		h = 0.1
 		for i in range(0,int(t/h)):
-			rk4Quaternion(mySat,f_constant,h)
+			updateStateTimeRK4(mySat,f_constant,h)
 			mySat.setTime(t0+(i+1)*h)
 		self.assertTrue(np.allclose([10.0,10.0,10.0],mySat.getW_BO_b()))
-	
-	def test_dynamics(self):
+
+	@data(0.001,0.005,0.05,0.1,0.5)	
+	def test_dynamics(self,value):
 		t0 = 0.
-		h = 0.001
+		h = value
 		
 		v_q_BO = np.array([0.4,0.254,-0.508,0.71931912])
 		
@@ -38,7 +41,7 @@ class TestSolver(unittest.TestCase):
 		mySat1.setDisturbance_b(np.array([10e-10,-4e-6,-3e-5]))
 		mySat1.setControl_b(np.array([1e-5,1e-5,-8e-4]))
 		
-		rk4Quaternion(mySat1,x_dot_BO,h)
+		updateStateTimeRK4(mySat1,x_dot_BO,h)
 		x1 = mySat1.getState()
 
 		mySat2 = satellite.Satellite(np.hstack((v_q_BO, v_w_BO_b)),t0)
@@ -52,13 +55,12 @@ class TestSolver(unittest.TestCase):
 		k1 = h*x_dot_BO(mySat2)
 		mySat2.setState(np.hstack((v_q_BO,v_w_BO_b))+0.5*k1)
 		k2 = h*x_dot_BO(mySat2)
-		mySat2.setState(np.hstack((v_q_BO,v_w_BO_b))+0.5*(k2+k1))
+		mySat2.setState(np.hstack((v_q_BO,v_w_BO_b))+0.5*(k2))
 		k3 = h*x_dot_BO(mySat2)
-		mySat2.setState(np.hstack((v_q_BO,v_w_BO_b))+k3+0.5*(k2+k1))
+		mySat2.setState(np.hstack((v_q_BO,v_w_BO_b))+k3)
 		k4 = h*x_dot_BO(mySat2)
 		
 		error_state = np.hstack((v_q_BO,v_w_BO_b)) + (1./6.)*(k1 + 2.*k2 + 2.*k3 + k4)
-		print(error_state)
 		self.assertTrue(np.allclose(error_state,x1))
 	
 if __name__=="__main__":
